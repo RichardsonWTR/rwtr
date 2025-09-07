3d3df2023aca043b12c1295e26b46546cb825673
Richardson Rosa <richardsonwtr@gmail.com>
2025-09-07 09:42:24 -0300

    refactor: placing the site name in all page titles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my blog built with Astro';
+export const SITE_TITLE = 'rwtr';
+export const SITE_DESCRIPTION = 'A blog dedicated to sharing practical insights, best practices, and experiences from my journey as a software developer.';
